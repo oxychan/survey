cd363e288fd97f289133f68a79c30bfa33698eff
rikysk <rickyseptia18@gmail.com>
2022-12-03 17:54:18 +0700

    edit form

--- resources/views/user/survey.blade.php
+++ resources/views/user/survey.blade.php
@@ -12,59 +12,31 @@
                                     <h4 class="card-title">Default Radio</h4>
                                 </div>
                                 <div class="card-body">
-                                    {{-- <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="flexRadioDefault"
-                                            id="flexRadioDefault1" />
-                                        <label class="form-check-label" for="flexRadioDefault1">
-                                            Default radio
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="flexRadioDefault"
-                                            id="flexRadioDefault2" checked />
-                                        <label class="form-check-label" for="flexRadioDefault2">
-                                            Default checked radio
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="flexRadioDisabled"
-                                            id="flexRadioDisabled" disabled />
-                                        <label class="form-check-label" for="flexRadioDisabled">
-                                            Disabled radio
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="flexRadioDisabled"
-                                            id="flexRadioCheckedDisabled" checked disabled />
-                                        <label class="form-check-label" for="flexRadioCheckedDisabled">
-                                            Disabled checked radio
-                                        </label>
-                                    </div> --}}
                                     <form action="action_page.php">
                                         <div class="form-group">
-                                            <label for="NAMA RESPONDEN">NAMA RESPONDEN:</label>
-                                            <input type="text" class="form-control" id="NAMA RESPONDEN" placeholder=""
-                                                name="NAMA RESPONDEN">
+                                            <label for="nama_responden">NAMA RESPONDEN:</label>
+                                            <input type="text" class="form-control" id="nama_responden" placeholder=""
+                                                name="nama_responden">
                                         </div>
 
                                         <div class="form-group">
                                             <label for="jenis_pelayanan">JENIS PELAYANAN::</label></P>
                                             <input type='radio' name='jenis_pelayanan'
-                                                value='Pengambilan Produk Pengadilan' />Pengambilan Produk Pengadilan&nbsp
+                                                value='Pengambilan_Produk_Pengadilan' />Pengambilan Produk Pengadilan&nbsp
                                             <input type='radio' name='jenis_pelayanan'
-                                                value='Pendaftaran Perkara' />Pendaftaran Perkara&nbsp
-                                            <input type='radio' name='jenis_pelayanan' value='Layanan Informasi' />Layanan
+                                                value='Pendaftaran_Perkara' />Pendaftaran Perkara&nbsp
+                                            <input type='radio' name='jenis_pelayanan' value='Layanan_Informasi' />Layanan
                                             Informasi&nbsp
                                             <input type='radio' name='jenis_pelayanan'
-                                                value='Layanan Pendaftaran' />Layanan Pendaftaran&nbsp
+                                                value='Layanan_Pendaftaran' />Layanan Pendaftaran&nbsp
                                             <span></span>
                                             </label>
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="TANGGAL SURVEY">TANGGAL SURVEY:</label>
-                                            <input type="date" class="form-control" id="TANGGAL SURVEY" placeholder=""
-                                                name="TANGGAL SURVEY">
+                                            <label for="tanggal_survey">TANGGAL SURVEY:</label>
+                                            <input type="date" class="form-control" id="tanggal_survey" placeholder=""
+                                                name="tanggal_survey">
                                         </div>
 
                                         <div class="form-group">
@@ -74,57 +46,57 @@
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="JENIS KELAMIN">JENIS KELAMIN:</label></P>
+                                            <label for="jenis_kelamin">JENIS KELAMIN:</label></P>
                                             <input type='radio' name='gender' value='male'>laki-laki&nbsp
                                             <input type='radio' name='gender' value='female'>perempuan&nbsp
                                             <span></span>
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="PENDIDIKAN TERAKHIR">PENDIDIKAN TERAKHIR:</label></P>
-                                            <input type='radio' name='PENDIDIKAN TERAKHIR' value='SD' />SD&nbsp
-                                            <input type='radio' name='PENDIDIKAN TERAKHIR' value='SMP' />SMP&nbsp
-                                            <input type='radio' name='PENDIDIKAN TERAKHIR' value='SMA' />SMA&nbsp
-                                            <input type='radio' name='PENDIDIKAN TERAKHIR' value='D1, D2, D3, D4' />D1,
+                                            <label for="pendidikan_terakhir">PENDIDIKAN TERAKHIR:</label></P>
+                                            <input type='radio' name='pendidikan_terakhir' value='SD' />SD&nbsp
+                                            <input type='radio' name='pendidikan_terakhir' value='SMP' />SMP&nbsp
+                                            <input type='radio' name='pendidikan_terakhir' value='SMA' />SMA&nbsp
+                                            <input type='radio' name='pendidikan_terakhir' value='D1, D2, D3, D4' />D1,
                                             D2, D3, D4&nbsp
-                                            <input type='radio' name='PENDIDIKAN TERAKHIR' value='S1' />S1&nbsp
-                                            <input type='radio' name='PENDIDIKAN TERAKHIR' value='S2' />S2&nbsp
-                                            <input type='radio' name='PENDIDIKAN TERAKHIR' value='S3' />S3&nbsp
-                                            <input type='radio' name='PENDIDIKAN TERAKHIR' value='TIDAK SEKOLAH' />TIDAK
+                                            <input type='radio' name='pendidikan_terakhir' value='S1' />S1&nbsp
+                                            <input type='radio' name='pendidikan_terakhir' value='S2' />S2&nbsp
+                                            <input type='radio' name='pendidikan_terakhir' value='S3' />S3&nbsp
+                                            <input type='radio' name='pendidikan_terakhir' value='TIDAK_SEKOLAH' />TIDAK
                                             SEKOLAH&nbsp
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="PEKERJAAN">PEKERJAAN:</label></P>
-                                            <input type='radio' name='PEKERJAAN'
+                                            <label for="pekerjaan">PEKERJAAN:</label></P>
+                                            <input type='radio' name='pekerjaan'
                                                 value='PNS/TNI/POLRI' />PNS/TNI/POLRI&nbsp
-                                            <input type='radio' name='PEKERJAAN' value='PEGAWAI SWASTA' />PEGAWAI
+                                            <input type='radio' name='pekerjaan' value='PEGAWAI SWASTA' />PEGAWAI
                                             SWASTA&nbsp
-                                            <input type='radio' name='PEKERJAAN'
+                                            <input type='radio' name='pekerjaan'
                                                 value='WIRASWASTA/WIRAUSAHA' />WIRASWASTA/WIRAUSAHA&nbsp
-                                            <input type='radio' name='PEKERJAAN'
+                                            <input type='radio' name='pekerjaan'
                                                 value='PELAJAR/MAHASISWA' />PELAJAR/MAHASISWA&nbsp
 
                                             <div class="form-group">
                                                 <br />
-                                                <b><label for="YANG LAIN">YANG LAIN:</label></b>
-                                                <input type="text" class="form-control" id="YANG LAIN" placeholder=""
-                                                    name="YANG LAIN">
+                                                <b><label for="yang_lain">YANG LAIN:</label></b>
+                                                <input type="text" class="form-control" id="yang_lain" placeholder=""
+                                                    name="yang_lain">
                                             </div>
 
                                             <div class="form-group">
-                                                <label for="SUKU BANGSA">SUKU BANGSA:</label></P>
-                                                <input type='radio' name='SUKU BANGSA' value='JAWA' />JAWA&nbsp
-                                                <input type='radio' name='SUKU BANGSA' value='MADURA' />MADURA&nbsp
-                                                <input type='radio' name='SUKU BANGSA' value='SUNDA' />SUNDA&nbsp
-                                                <input type='radio' name='SUKU BANGSA' value='BATAK' />BATAK&nbsp
-                                                <input type='radio' name='SUKU BANGSA' value='BANJAR' />BANJAR&nbsp
+                                                <label for="suku_bangsa">SUKU BANGSA:</label></P>
+                                                <input type='radio' name='suku_bangsa' value='JAWA' />JAWA&nbsp
+                                                <input type='radio' name='suku_bangsa' value='MADURA' />MADURA&nbsp
+                                                <input type='radio' name='suku_bangsa' value='SUNDA' />SUNDA&nbsp
+                                                <input type='radio' name='suku_bangsa' value='BATAK' />BATAK&nbsp
+                                                <input type='radio' name='suku_bangsa' value='BANJAR' />BANJAR&nbsp
 
                                                 <div class="form-group">
                                                     <br />
-                                                    <b><label for="YANG LAIN">YANG LAIN:</label></b>
-                                                    <input type="text" class="form-control" id="YANG LAIN"
-                                                        placeholder="" name="YANG LAIN">
+                                                    <b><label for="yang_lain">YANG LAIN:</label></b>
+                                                    <input type="text" class="form-control" id="yang_lain"
+                                                        placeholder="" name="yang_lain">
                                                 </div>
 
                                                 <div class="checkbox">
